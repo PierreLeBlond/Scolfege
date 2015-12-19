95edc6dfe04a0f3c25d716591d607141964372f6
PierreLeBlond <pierre.lespingal@gmail.com>
2015-12-19 10:58:41 +0100

    First commit

---
+++ Assets/Script/Chord.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Chord : MonoBehaviour {
+
+	public Key keyPrefab;
+
+	public Vector2 speed = new Vector2 (3f, 0f);
+	public Vector2 direction = new Vector2 (-1f, 0f);
+
+	private List<Key> keys = new List<Key>();
+
+	private int keyName;
+
+	private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+		transform.localPosition = new Vector3(13f, 0f, 0f);
+	}
+
+    public void generateKeys()
+    {
+        Key key;
+        int j = Random.Range(0, 8);
+        int rightKey = Random.Range(0, 3);
+        for (int i = 0; i < 3; i++)
+        {
+            key = Instantiate(keyPrefab) as Key;
+            key.transform.localPosition = new Vector3(0f, -1.5f + j * 0.5f + i * 1f, 0.0f);
+            if(j + 2*i < 0 || j + 2*i > 10 && (j + 2*i) % 2 != 0)
+            {
+                key.setKeyOut();
+            }
+            key.transform.parent = transform;
+            keys.Add(key);
+            if (i == rightKey)
+            {
+                keyName = (j + 2 * i + 1) % 7;
+                key.setRight(true);
+            }
+        }
+    }
+
+	public int getRightKeyName(){
+		return keyName;
+	}
+
+    public List<Key> getKeys()
+    {
+        return keys;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		movement = new Vector2 (
+			speed.x * direction.x,
+			speed.y * direction.y);
+	}
+
+	void FixedUpdate () {
+		GetComponent<Rigidbody2D>().velocity = movement;
+	}
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+	public GameObject defaultSkin;
+    public GameObject winSkin;
+    public GameObject looseSkin;
+    public GameObject assSkin;
+    public GameObject sickSkin;
+
+    public Text _scoreText;
+
+    private int _score = 0;
+
+    private bool _hasTheChord = false;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void win ()
+    {
+        _score += 1;
+        updateScore();
+        _hasTheChord = true;
+        defaultSkin.SetActive(false);
+        sickSkin.SetActive(false);
+        winSkin.SetActive(true);
+        assSkin.SetActive(false);
+        StartCoroutine(defaultState());
+    }
+
+    public void loose ()
+    {
+        _score -= 1;
+        updateScore();
+        _hasTheChord = true;
+        defaultSkin.SetActive(false);
+        sickSkin.SetActive(false);
+        looseSkin.SetActive(true);
+        assSkin.SetActive(false);
+        StartCoroutine(defaultState());
+    }
+
+    public void showAss()
+    {
+        defaultSkin.SetActive(false);
+        sickSkin.SetActive(false);
+        looseSkin.SetActive(false);
+        assSkin.SetActive(true);
+        StartCoroutine(defaultState());
+    }
+
+    public bool HasTheChord()
+    {
+        return _hasTheChord;
+    }
+
+    public void setHasTheChord(bool b)
+    {
+        _hasTheChord = b;
+    }
+
+    private void updateScore()
+    {
+        _scoreText.text = _score.ToString();
+        if(_score < 0)
+        {
+            _scoreText.color = Color.red;
+        }
+        else
+        {
+            _scoreText.color = Color.white;
+        }
+    }
+
+    public IEnumerator defaultState ()
+    {
+        yield return new WaitForSeconds(2);
+        winSkin.SetActive(false);
+        looseSkin.SetActive(false);
+        assSkin.SetActive(false);
+        if(_score < 0)
+        {
+            defaultSkin.SetActive(false);
+            sickSkin.SetActive(true);
+        }
+        else
+        {
+            defaultSkin.SetActive(true);
+            sickSkin.SetActive(false);
+        }
+        
+    }
+}
